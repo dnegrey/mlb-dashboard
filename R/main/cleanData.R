3b3905b1a47de2f27b29a50f445db04863a56845
dnegrey <dnegrey@gmail.com>
2016-09-26 09:31:15 -0400

    added divs class to yearly data frames

--- R/main/cleanData.R
+++ R/main/cleanData.R
@@ -7,14 +7,19 @@ cleanData <- function(dir) {
         fun = function(x){
             lapply(split(x, x$yearID),
                    function(x){
-                       select(x, yearID, lgID, teamID, franchID, divID, Rank,
-                              G, Ghome, W, L, WSWin, LgWin, DivWin, WCWin, name) %>% 
+                       y <- select(x, yearID, lgID, teamID, franchID, divID, Rank,
+                                   G, Ghome, W, L, WSWin, LgWin, DivWin, WCWin, name) %>% 
                            mutate(lgID = as.character(lgID),
                                   teamID = as.character(teamID),
                                   franchID = as.character(franchID)) %>% 
                            mutate(LeagueDiv = ifelse(is.na(divID),
                                                      lgID,
                                                      paste0(lgID, divID)))
+                       divs <- unique(y$divID)
+                       divs <- divs[order(divs)]
+                       divs <- paste(divs, collapse = "")
+                       class(y) <- c(class(y), sprintf("divs%s", divs))
+                       return(y)
                    })
         },
         args = list(x = RDataUse(dir, "teams"))
