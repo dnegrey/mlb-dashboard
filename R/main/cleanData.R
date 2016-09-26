a5b42e95242f46f10a21a4a3fc336f5b704ff830
dnegrey <dnegrey@gmail.com>
2016-09-25 23:51:55 -0400

    split teams by year

--- R/main/cleanData.R
+++ R/main/cleanData.R
@@ -1,2 +1,23 @@
 cleanData <- function(dir) {
+    
+    # years
+    RDataCreate(
+        dir = dir,
+        x = "years",
+        fun = function(x){
+            lapply(split(x, x$yearID),
+                   function(x){
+                       select(x, yearID, lgID, teamID, franchID, divID, Rank,
+                              G, Ghome, W, L, WSWin, LgWin, DivWin, WCWin, name) %>% 
+                           mutate(lgID = as.character(lgID),
+                                  teamID = as.character(teamID),
+                                  franchID = as.character(franchID)) %>% 
+                           mutate(LeagueDiv = ifelse(is.na(divID),
+                                                     lgID,
+                                                     paste0(lgID, divID)))
+                   })
+        },
+        args = list(x = RDataUse(dir, "teams"))
+    )
+    
 }
