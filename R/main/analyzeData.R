5f2c171bbbb3145a46a3ccb2f3c6006e880fe8fb
dnegrey <dnegrey@gmail.com>
2016-10-05 22:39:04 -0400

    completed yearResults function

--- R/main/analyzeData.R
+++ R/main/analyzeData.R
@@ -1,2 +1,14 @@
 analyzeData <- function(dir) {
+    
+    # year results
+    RDataCreate(
+        dir = dir,
+        x = "results",
+        fun = lapply,
+        args = list(
+            X = RDataUse(dir, "years"),
+            FUN = yearResults
+        )
+    )
+    
 }
