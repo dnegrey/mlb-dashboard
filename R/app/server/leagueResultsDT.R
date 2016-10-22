9c7dcec8be75a110d28fa9b89319a79129a4c4ef
dnegrey <dnegrey@gmail.com>
2016-10-21 23:04:00 -0400

    division results in leagueResultsDT

--- R/app/server/leagueResultsDT.R
+++ R/app/server/leagueResultsDT.R
@@ -8,10 +8,41 @@ leagueResultsDT <- function(results) {
         X = x,
         FUN = function(x){
             lapply(x, function(x){
-                x %>% 
-                    mutate(Result = ifelse(WD, "*", ifelse(WC, "**", NA)),
-                           PCT = round(PCT, 3)) %>% 
-                    select(-Rank, -WD, -WC)
+                x$CGB <- ifelse(is.na(x$GB), 0, x$GB)
+                x$PGB <- lag(x$CGB, default = -1)
+                x$NewRank <- 1:nrow(x)
+                x$NewRank <- ifelse(x$CGB == x$PGB, "-", as.character(x$NewRank))
+                x <- x %>% 
+                    mutate(WD = ifelse(is.na(WD), FALSE, WD),
+                           WC = ifelse(is.na(WC), FALSE, WC),
+                           Result = ifelse(WD, "*", ifelse(WC, "**", "")),
+                           PCT = paste0(".", 1000*round(PCT, 3)),
+                           GB = ifelse(is.na(GB), "-", format(round(GB, 1), nsmall = 1))) %>% 
+                    select(-Rank, -WD, -WC) %>% 
+                    mutate(W = as.character(W),
+                           L = as.character(L)) %>% 
+                    select(
+                        Rank = NewRank,
+                        Team,
+                        W,
+                        L,
+                        PCT,
+                        GB,
+                        DIV_WC = Result
+                    )
+                y <- as.data.frame(lapply(x[1, ], function(x){""}))
+                x <- rbind(x, y)
+                y <- as.data.frame(rbind(names(y)), stringsAsFactors = FALSE)
+                names(y) <- names(x)
+                y$Rank <- "#"
+                y$DIV_WC <- "DIV/WC"
+                for (i in 1:length(y)){
+                    y[1, i] <- paste0("<span class=\"leagueResultsDT-header\">",
+                                      y[1, i],
+                                      "</span>")
+                }
+                x <- rbind(y, x)
+                return(x)
             })
         }
     )
